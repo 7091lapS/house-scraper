a658f8a68c48fb8c61b43175437c8e244400e508
Antonio Voce <antonio.voce@guardian.co.uk>
2020-02-17 22:08:37 +0000

    fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ curl.setHeaders([
   'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
 ])
 .get(testLink)
-.then(({ statusCode, body, headers }) => {
+.then(({ body }) => {
   const dom = new JSDOM(body)
   const refs = [...dom.window.document.querySelectorAll('.propertyCard-link')].map(d => "https://www.rightmove.co.uk" + d.href)
   
@@ -57,5 +57,5 @@ curl.setHeaders([
     })
 })
 .catch((e) => {
-  console.log('error');
+  console.log(e);
 });
